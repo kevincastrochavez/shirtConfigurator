c98c2a4ef11ad838b1a87c81c2bfa61ae1d157e5
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-11-13 10:42:54 -0700

    Created CameraRig component to rotate object on mouse movement

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { Environment, Center, OrbitControls } from '@react-three/drei';
 import Shirt from './components/Shirt';
 import Backdrop from './components/Backdrop';
+import CameraRig from './components/CameraRig';
 
 const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
   <Canvas
@@ -14,10 +15,12 @@ const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
     <ambientLight intensity={0.5} />
     <Environment files='https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr' />
 
-    <Center>
-      <Shirt />
-      <Backdrop />
-    </Center>
+    <CameraRig>
+      <Center>
+        <Shirt />
+        <Backdrop />
+      </Center>
+    </CameraRig>
     <OrbitControls />
   </Canvas>
 );
