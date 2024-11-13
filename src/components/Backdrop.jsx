8057e0ceca41d3705e4abce9f585a290890bceb2
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-11-13 11:27:15 -0700

    Modified backdrop lights

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -1,26 +1,24 @@
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
+import { useRef } from 'react';
 
 function Backdrop() {
+  const shadows = useRef();
+
   return (
     <AccumulativeShadows
+      ref={shadows}
       temporal
       frames={60}
       alphaTest={0.85}
-      scale={10}
+      scale={1.5}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
+      <RandomizedLight amount={4} radius={9} intensity={3} ambient={0.25} />
       <RandomizedLight
-        amount={4}
-        radius={9}
-        intensity={0.55}
-        ambient={0.25}
-        position={[5, 5, -10]}
-      />
-      <RandomizedLight
-        amount={4}
+        amount={1}
         radius={5}
-        intensity={0.25}
+        intensity={1}
         ambient={0.55}
         position={[-5, 5, -9]}
       />
